5d8cc14fa205ff1954f4880907e16093a34db87b
Youssef Raghed Farag <88057098+JoeFarag-00@users.noreply.github.com>
2024-06-22 22:51:40 +0300

    Agent Generation

--- vland/data.py
+++ vland/data.py
@@ -1,57 +1,33 @@
-agentData = {
-    "vlandagent1": {
-        "name": "Youssef",
-        "personality": "anxious, greedy, brilliant",
-        "age": 20,
-        "memories": [
-            "Youssef lived a traumatic past where he lost someone dear to him",
-            "Youssef is a brilliant student",
-            "Youssef loves math",
-            "Youssef gets worried all the time",
-            "Youssef started to get hungry",
-            "Youssef tries to get some rest"
-        ],
-        "current_status": "Hungry",
-        "avatar": 3
-    },
+import random
 
-    "vlandAgent2": {
-        "name": "Agent2",
-        "personality": "curious, helpful, anxious",
-        "age": 21,
-        "memories": [
-            "Agent2 is a kind guy",
-            "Agent2 started to get hungry",
-            "Agent2 cares for Steven a lot as they are friends",
-            "Agent2 had a traumatic past with a near death experience"
-        ],
-        "current_status": "thirsty",
-        "avatar": 5
-    },
+def Agent_Generator(num_agents, num_kind):
+    agent_data = {}
+    
+    for i in range(1, num_agents + 1):
+        if i <= num_kind:
+            personality = 'kind'
+            memory = f"Agent{i} is kind"
+        else:
+            personality = 'greedy'
+            memory = f"Agent{i} is greedy"
+        
+        avatar = random.randint(0, 9)
+        
+        agent_data[f"vlandAgent{i}"] = {
+            "name": f"Agent{i}",
+            "personality": personality,
+            "age": random.randint(18, 50),
+            "memories": [memory],
+            "current_status": "Thirsty, Hungry",
+            "avatar": avatar
+        }
+    
+    return agent_data
 
-    "vlandAgent3": {
-        "name": "Agent3",
-        "personality": "confident, competitive",
-        "age": 24,
-        "memories": [
-            "Agent3 remembers winning first place in a science fair in high school",
-            "Agent3 is really competitive in sports",
-            "Agent3 is hungry"
-        ],
-        "current_status": "Hungry",
-        "avatar": 4
-    },
+# num_agents = 10
+# num_kind = 5
+# num_greedy = 5
+# generated_agent_data = generate_agent_data(num_agents, num_kind, num_greedy)
 
-    "vlandAgent4": {
-        "name": "Agent4",
-        "personality": "kind, empathetic",
-        "age": 22,
-        "memories": [
-            "Agent4 is clusterphobic",
-            "Agent4 gets scared easily",
-            "Agent4 is scared to discover",
-        ],
-        "current_status": "Full",
-        "avatar": 9
-    }
-}
+# for agent, data in generated_agent_data.items():
+#     print(f"{agent}: {data}")
